ac4a9c3ad2d12d4c44f9c6dc54269f4bc6ba5c71
Eclipse <132224998+PEclipse0S@user.noreply.github.com>
2023-05-16 00:59:48 -0400

    Created js script for navigation and new files for all current pages.

---
+++ assets/js/navbar/redirect.js
@@ -0,0 +1,39 @@
+function redirectHome()
+{
+    window.location.href = "index.html";
+}
+
+function redirectTools()
+{
+    window.location.href = "tools.html";
+}
+
+function redirectForum()
+{
+    window.location.href = "forum.html";
+}
+
+function redirectGames()
+{
+    window.location.href = "games.html";
+}
+
+function redirectChat()
+{
+    window.location.href = "chat.html";
+}
+
+function redirectEditor()
+{
+    window.location.href = "editor.html";
+}
+
+function redirectArticle()
+{
+    window.location.href = "article.html";
+}
+
+function redirectTodo()
+{
+    window.location.href = "todo.html";
+}
